107b1f7be0810e463aeed8a5ff1d58e11f2c6417
Ilma Ismail <139671858+IlmaIsmail@users.noreply.github.com>
2024-11-21 01:18:07 +0000

    passing artist component into app

--- day4/src/App.js
+++ day4/src/App.js
@@ -1,10 +1,12 @@
  import './App.css';
  import Fruits from './components/Fruits';
+ import Artist from './components/Artist';
 
 function App() {
   return (
     <div className="App">
-     <Fruits></Fruits>   
+     <Fruits></Fruits>  
+     <Artist></Artist> 
      
     </div>
   );
